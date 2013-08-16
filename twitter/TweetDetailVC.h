8c57e4adc52d2ec3250d1b008a0423d4fa42cc0e
Seema Kamath <seema.kamath@gmail.com>
2013-08-15 22:50:04 -0700

    added TweetDetail

---
+++ twitter/TweetDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  TweetDetailVC.h
+//  twitter
+//
+//  Created by Seema Kamath on 8/15/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TweetDetailVC : UIViewController
+
+@end
